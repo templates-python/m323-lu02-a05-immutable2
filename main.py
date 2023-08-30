d51f2e2d083135a6ecce4c0e958a64d4a0365f03
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 11:24:49 +0200

    done

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 numbers = [1, 2, 3, 4, 5]
 
-def increment_numbers(numbers):
-    # Your code here
-    pass
 
-if __name__ == '__main__':
-    # Your code here
-    pass
+def increment_numbers(numbers):
+    new_numbers = []  # create an empty list to store the new numbers
+    for number in numbers:
+        new_numbers.append(number + 1)  # append the incremented value to the new list
+    return new_numbers
+
+
+if __name__ == "__main__":
+    new_numbers = increment_numbers(numbers)
+    print("Original numbers:", numbers)
+    print("Incremented numbers:", new_numbers)
